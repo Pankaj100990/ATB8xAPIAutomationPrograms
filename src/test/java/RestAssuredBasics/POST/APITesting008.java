a70311a746941022105394002784931768cfa36b
PANKAJG-TECH\Pankaj Gokhale <pankaj.gokhale@rebelfoods.com>
2024-12-01 14:11:34 +0530

    Allure Report-BDD

---
+++ src/test/java/RestAssuredBasics/POST/APITesting008.java
@@ -0,0 +1,38 @@
+package RestAssuredBasics.POST;
+
+import io.qameta.allure.Description;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+public class APITesting008 {
+    @Description("Verify the POST Request - BDD Style")
+    @Test
+    public void test_post_BDDStyle() {
+
+        String payload  = "{\n" +
+                "    \"username\" : \"admin\",\n" +
+                "    \"password\" : \"password123\"\n" +
+                "}";
+
+        RestAssured
+                .given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/auth")
+                .contentType(ContentType.JSON)
+                .log().all().body(payload)
+                .when()
+                .log().all()
+                .post()
+                .then().log().all()
+                .statusCode(203);
+
+
+
+
+
+
+
+    }
+
+}
